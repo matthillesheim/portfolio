e02990a96b9b418de93e14d62a953a9cc04ac0d5
Ravi <raviraval@gmail.com>
2018-09-01 18:45:19 -0700

    image grid, overlay prep

--- assets/js/new_main.js
+++ assets/js/new_main.js
@@ -1,1 +1,6 @@
 const imageWrappers = document.querySelectorAll('.image-wrapper');
+
+function toggleOverlay() {
+	el = document.getElementById("overlay");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
